c6f2eb8816e6732f0c0e9fb717a9082d360959ca
Eron Salling <esalling23@gmail.com>
2025-07-22 10:30:57 -0400

    Updates next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  // Use the name of your repository as the basePath
+  basePath: isProd ? '/ModuleTemplate' : '',
+  // Also use the repository name for the assetPrefix
+  assetPrefix: isProd ? '/ModuleTemplate/' : '',
   output: 'export',
+  images: {
+    unoptimized: true, // Required for static export
+  }
 };
 
 export default nextConfig;
